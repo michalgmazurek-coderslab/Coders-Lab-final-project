26503e7ccc6635014f418eb0d4826623ef658461
Michał Mazurek <michalgmazurek@gmail.com>
2017-03-17 09:48:23 +0100

    In progress

--- app.js
+++ app.js
@@ -3,15 +3,31 @@ document.addEventListener("DOMContentLoaded", function (event) {
     console.log(playerCar);
     var playerCarStyle = playerCar.style.left;
     var playerCarStyleNumber = Number(playerCarStyle);
+    
     console.log(playerCarStyle);
+    
+     var addLeft = function(x) {
+        playerCar.css()("left", (playerCarStyle + x) + "vw");
+        
+    };
+    
+    
     document.addEventListener("keydown", function (event) {
-        if (event.keyCode === 39) {
-            console.log("right");
-            playerCar.style.left = (playerCarStyleNumber + 1) + "%";
+        var offset = 12;
+        var pos = $("#playerCar").position();
+        console.log(pos);
+        if(event.keyCode == '39') {    
+            $("#playerCar").css('left', pos.left + offset);
         }
-        else if (event.keyCode === 37) {
-            console.log("left");
-            playerCar.style.left = (playerCarStyleNumber - 1) + "%";
+        else if(event.keyCode == '37') {
+            $("#playerCar").css('left',pos.left - offset);
         }
+        else if(event.keyCode == '38') {
+            $("#playerCar").css('top',pos.top - offset);
+        }
+        else if(event.keyCode == '40') {
+            $("#playerCar").css('top',pos.top + offset);
+        }
+    
     });
 });
