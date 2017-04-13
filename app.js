88e93895c0293a9e4e651e31ca96225c04db751c
Michał Mazurek <michalgmazurek@gmail.com>
2017-04-13 10:09:43 +0200

    In Progress

--- app.js
+++ app.js
@@ -62,8 +62,14 @@ document.addEventListener("DOMContentLoaded", function(event) {
                 }
             }
         }
+    };
 
-    }
+    $("body").bind("touchend", function(e) {
+      e.preventDefault();
+      // Add your code here.
+      $(this).click();
+      // This line still calls the standard click event, in case the user needs to interact with the element that is being clicked on, but still avoids zooming in cases of double clicking.
+    })
 
     turnLeftButton.mousedown(function(event) {
         var offset = lineWidth;
